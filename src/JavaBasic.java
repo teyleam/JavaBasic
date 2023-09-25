2d9f04419be4be06693a3d04c450512891cdb79f
Teyleam <huynhtrunglam167@gmail.com>
2023-09-25 22:03:12 +0700

    Initial commit

---
+++ src/JavaBasic.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class JavaBasic {
+        //function
+        public static void main(String[] args) {
+            //kieu du lieu
+            //String : kiểu chuỗi => 2 ký tự trở lên
+            // int : kiểu số nguyên => đại diện cho số
+            // float : kiểu số thực => đại diện số thập phân
+            // boolean : kiểu lý luận (1: true , 2 : false)
+
+            //khai báo biến
+            //type variable = value
+//        int age = 28;
+//       String address = "Quan 1";
+//       boolean isMale = true;
+//       float height = 1.75f;
+//       System.out.println(age+address+isMale+height);
+
+            //a = 5, b = 6
+            // ++a - --b - --a - b-- + a-- + b-- - b++
+            // 6 - 5 - --a - b-- + a-- + b-- - b++ , a = 6, b = 5
+            // 6 - 5 - 5 - 5 + a-- + b-- - b++ , a = 5, b = 4
+            // 6 - 5 - 5 - 5 + 5 + 4 - b++ , a = 4, b = 3
+            // 6 - 5 - 5 - 5 + 5 + 4 - 3 , a = 4, b = 4
+            //KQ = -3
+//        int total = 9000;
+//        if (total > 500000){
+//            total = total * 80/100;
+//            System.out.println("Giam gia 20%" + total);
+//        }else if(total >=200000 && total<=500000){
+//            total = total * 85/100;
+//            System.out.println("Giam gia 15%" + total);
+//        }else{
+//            total = total * 90/100;
+//            System.out.println("Giam 10% " + total);
+//        }
+//        String message = "";
+//
+//       Scanner height = new Scanner(System.in);
+//       String Height  = height.nextLine();
+
+//        Scanner weight = new Scanner(System.in);
+//        String Weight  = weight.nextLine();
+//        float BMI = weight/(weight*weight).toString();
+//        if (BMI>0) {
+//            if (BMI >= 40) {
+//                message = "Beo phi do III";
+//            } else if (BMI >= 35 && BMI <= 39.9) {
+//                message = "Beo phi do II";
+//            } else if (BMI >= 30 && BMI <= 34.9) {
+//                message = "Beo phi do I";
+//            } else if (BMI >= 25) {
+//                message = "Thua Can";
+//            } else if (BMI >= 18.5 && BMI <= 24.9) {
+//                message = "Binh Thuong";
+//            } else {
+//                message = "Gay";
+//            }
+//        }else{
+//            message = "Loi";
+            // }
+//        System.out.println(message);
+//        System.out.println(BMI);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhap chieu cao : ");
+//
+//        String userName = scanner.nextLine();
+//        System.out.println("Chieu cao la "+userName);
+
+            //mang : array
+            //Cach 1: Khai bao mang nhung chi biet kich thuoc mang
+//        int[] arrNumber = new int[5]; // mang co 5 phan tu
+//
+//        //Cach 2: biet gia tri cu the gia tri phan tu
+//
+//        String[] arrNames = {"Teo", "Ti", "Lam"};
+        }
+    }
+
+    
